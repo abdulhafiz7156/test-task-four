b93b32a230fa16705296622c0e3819cd4ce73f06
abdulhafiz <xxafiz69@gmail.com>
2025-02-19 14:54:03 +0500

    last updates

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
-/// <reference types="vite/client" />
+declare module '*.vue' {
+    import { DefineComponent } from 'vue';
+    const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>;
+    export default component;
+}
